b936d9b2e078bd571e9f3651ac09ac39c3f9f84d
thomasalcock <thomas.alcock@statworx.com>
2025-05-03 18:16:31 +0200

    refactor

--- source/app.d
+++ source/app.d
@@ -2,6 +2,10 @@ import std.stdio;
 import std.conv;
 import raylib;
 
+import utils;
+
+// TODO: make window resizeable
+// TODO: display and fade out text / state when user changes input
 void main()
 {
 	const int screenWidth = 800;
@@ -29,12 +33,7 @@ void main()
 		fileDropped = IsFileDropped();
 		if (fileDropped)
 		{
-			fileCounter++;
-			FilePathList droppedFile = LoadDroppedFiles();
-			char* filePath = droppedFile.paths[0];
-			image = LoadImageAnim(filePath, &totalFrames);
-			texture = LoadTextureFromImage(image);
-			UnloadDroppedFiles(droppedFile);
+			loadTextureFromGif(fileCounter, totalFrames, image, texture);
 		}
 
 		if (IsKeyPressed(KeyboardKey.KEY_SPACE))
@@ -48,54 +47,17 @@ void main()
 
 		if (updateTexture && fileCounter > 0)
 		{
-			frameCounter++;
-			if (frameCounter >= frameDelay)
-			{
-				if (reverse)
-				{
-					currentAnimFrame--;
-					if (currentAnimFrame < 0)
-					{
-						currentAnimFrame = totalFrames - 1;
-					}
-				}
-				else
-				{
-					currentAnimFrame++;
-					if (currentAnimFrame >= totalFrames)
-					{
-						currentAnimFrame = 0;
-					}
-				}
-				nextFrameDataOffset = image.width * image.height * 4 * currentAnimFrame;
-				UpdateTexture(texture, (image.data) + nextFrameDataOffset);
-				frameCounter = 0;
-			}
+			updateGifFrame(frameCounter, frameDelay, currentAnimFrame, totalFrames,
+				reverse, nextFrameDataOffset, image, texture);
 		}
 
-		if (IsKeyPressed(KeyboardKey.KEY_DOWN))
-		{
-			frameDelay++;
-		}
-		else if (IsKeyPressed(KeyboardKey.KEY_UP))
-		{
-			frameDelay--;
-		}
-
-		if (frameDelay > MAX_FRAME_DELAY)
-		{
-			frameDelay = MAX_FRAME_DELAY;
-		}
-		else if (frameDelay < MIN_FRAME_DELAY)
-		{
-			frameDelay = MIN_FRAME_DELAY;
-		}
+		changeGifSpeed(frameDelay, MIN_FRAME_DELAY, MAX_FRAME_DELAY);
 
 		BeginDrawing();
 
 		ClearBackground(Colors.DARKGRAY);
 		Rectangle source = Rectangle(0, 0, cast(float) texture.width, cast(float) texture.height);
-		Rectangle dest = Rectangle(0, 0, cast(float) screenWidth, cast(float) screenHeight);
+		Rectangle dest = Rectangle(0, 0, cast(float) GetScreenWidth(), cast(float) GetScreenHeight());
 
 		if (fileCounter > 0)
 		{
@@ -103,14 +65,7 @@ void main()
 		}
 		else
 		{
-
-			DrawText("Drag and drop a .gif file onto the screen", 100, 100, 20, Colors.RAYWHITE);
-			DrawText("Play the gif faster / slower by hitting up / down", 100, 150, 20, Colors
-					.RAYWHITE);
-			DrawText("Play / pause by hitting the spacebar", 100, 200, 20, Colors
-					.RAYWHITE);
-			DrawText("Reverse the gif by hitting the R key", 100, 250, 20, Colors
-					.RAYWHITE);
+			drawInstructions();
 		}
 		EndDrawing();
 	}
---
+++ source/utils.d
@@ -0,0 +1,82 @@
+module utils;
+
+import raylib;
+
+void loadTextureFromGif(
+    ref int fileCounter,
+    ref int totalFrames,
+    ref Image image,
+    ref Texture texture)
+{
+    fileCounter++;
+    FilePathList droppedFile = LoadDroppedFiles();
+    char* filePath = droppedFile.paths[0];
+    image = LoadImageAnim(filePath, &totalFrames);
+    texture = LoadTextureFromImage(image);
+    UnloadDroppedFiles(droppedFile);
+}
+
+void reverseGif(ref bool reverse, ref int currentAnimFrame, ref int totalFrames)
+{
+    if (reverse)
+    {
+        currentAnimFrame--;
+        if (currentAnimFrame < 0)
+        {
+            currentAnimFrame = totalFrames - 1;
+        }
+    }
+    else
+    {
+        currentAnimFrame++;
+        if (currentAnimFrame >= totalFrames)
+        {
+            currentAnimFrame = 0;
+        }
+    }
+}
+
+void updateGifFrame(ref int frameCounter, ref int frameDelay, ref int currentAnimFrame, ref int totalFrames,
+    ref bool reverse, ref uint nextFrameDataOffset, ref Image image, ref Texture texture)
+{
+    frameCounter++;
+    if (frameCounter >= frameDelay)
+    {
+        reverseGif(reverse, currentAnimFrame, totalFrames);
+        nextFrameDataOffset = image.width * image.height * 4 * currentAnimFrame;
+        UpdateTexture(texture, (image.data) + nextFrameDataOffset);
+        frameCounter = 0;
+    }
+}
+
+void drawInstructions()
+{
+    DrawText("Drag and drop a .gif file onto the screen", 100, 100, 20, Colors.RAYWHITE);
+    DrawText("Play the gif faster / slower by hitting up / down", 100, 150, 20, Colors
+            .RAYWHITE);
+    DrawText("Play / pause by hitting the spacebar", 100, 200, 20, Colors
+            .RAYWHITE);
+    DrawText("Reverse the gif by hitting the R key", 100, 250, 20, Colors
+            .RAYWHITE);
+}
+
+void changeGifSpeed(ref int frameDelay, const ref int MIN_FRAME_DELAY, const ref int MAX_FRAME_DELAY)
+{
+    if (IsKeyPressed(KeyboardKey.KEY_DOWN))
+    {
+        frameDelay++;
+    }
+    else if (IsKeyPressed(KeyboardKey.KEY_UP))
+    {
+        frameDelay--;
+    }
+    if (frameDelay > MAX_FRAME_DELAY)
+    {
+        frameDelay = MAX_FRAME_DELAY;
+    }
+    else if (frameDelay < MIN_FRAME_DELAY)
+    {
+        frameDelay = MIN_FRAME_DELAY;
+    }
+
+}
